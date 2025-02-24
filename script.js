dd7baafac97c7599895739b5890a67ffe3ccae3f
Papa Franku <158589681+PapaFrankuu@users.noreply.github.com>
2025-02-24 23:00:52 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleMenu(){
-    const menu=document.querySelector('.menu-links');
-    const icon=document.querySelector('.hamburger-icon');
-    menu.classList.toggle('open');
-    icon.classList.toggle('open');
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+const instaGif = document.querySelector(".insta-gif");
+// Add event listener to reset animation after it completes
+instaGif.addEventListener("animationend", () => {
+  instaGif.style.animationPlayState = "paused"; // Reset after animation ends
+});
